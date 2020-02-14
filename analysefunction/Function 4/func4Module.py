fbc0954fdfef3fd126b2bbdebe59b3cf698b3392
k-skaap <kwande.skaap@gmail.com>
2020-02-14 18:46:06 +0200

    Update func4Module.py

--- analysefunction/Function 4/func4Module.py
+++ analysefunction/Function 4/func4Module.py
@@ -32,42 +32,49 @@ def date_parser(dates):
     return storage  
 #end of function 3
 #function 4
+### START FUNCTION
 def extract_municipality_hashtags(df):
-
-  municipality_dict ={ '@CityofCTAlerts' : 'Cape Town',
-            '@CityPowerJhb' : 'Johannesburg',
-            '@eThekwiniM' : 'eThekwini' ,
-            '@EMMInfo' : 'Ekurhuleni',
-            '@centlecutility' : 'Mangaung',
-            '@NMBmunicipality' : 'Nelson Mandela Bay',
-            '@CityTshwane' : 'Tshwane'}
+    
     df_working = df.copy(deep = True)
-    finder_keys = list(municipality_dict.keys())
-    finder_values = list(municipality_dict.values())
+    finder_keys = list(mun_dict.keys())
+    finder_values = list(mun_dict.values())
     finder = '|'.join(finder_keys +finder_values)
     df_working.insert(2, 'municipality', df.Tweets.str.contains(finder).replace((True,False),('', np.nan)))
     df_working.insert(3, 'hashtags', df.Tweets.str.contains('#').replace((True, False),('', np.nan)))
     finder_municipality = df_working[df.Tweets.str.contains(finder) == True].index.to_list()
     finder_tags = df_working[df.Tweets.str.contains('#') == True].index.to_list()
-
+    
     for index in finder_tags:
         tempy = []
-        magic = df_working.at[index, 'Tweets'].split()
+        magic = df_working.at[index, 'Tweets'].lower().split()
         for items in magic:
             if items[0] == '#':
                 tempy.append(items)
                 df_working.at[index, 'hashtags'] = tempy
+            elif '#' in items:
+                stop=1
+                while stop !=-1:
+                    if items[stop] == '#':
+                        tempy.append(items[stop:])
+                        df_working.at[index, 'hashtags'] = tempy
+                        stop =-1
+                    else: stop+=1
     for index in finder_municipality:
         magic = df_working.at[index, 'Tweets'].split()
         for items in magic:
             if items in finder_keys:
-                df_working.at[index, 'municipality'] = municipality_dict[items]
-
-    for num in finder_municipality:
-        tweet = df_working.at[index, 'Tweets'].split()
-        for values in tweet:
-            if values in finder_values:
-                df_working.at[index, 'municipality'] = values
+                df_working.at[index, 'municipality'] = mun_dict[items]
+            elif items in finder_values:
+                df_working.at[index, 'municipality'] = items
+            elif items[-1]  == ':' :
+                finder_2 = items[:-1]
+                if finder_2 in finder_keys:
+                    df_working.at[index, 'municipality'] = mun_dict[finder_2]
+                elif finder_2 in finder_values:
+                    df_working.at[index, 'municipality'] = finder_2
+            
+                
+    
     return df_working
 #End of function 4
 
