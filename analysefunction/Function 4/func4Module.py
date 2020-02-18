d0fe20edd49d7da8108b36a189bf1a2db6f7735b
k-skaap <kwande.skaap@gmail.com>
2020-02-19 00:48:46 +0200

    Update func4Module.py
    Outline of final package

--- analysefunction/Function 4/func4Module.py
+++ analysefunction/Function 4/func4Module.py
@@ -1,3 +1,68 @@
+import pandas as pd
+import numpy as np
+
+ebp_url = 'https://raw.githubusercontent.com/Explore-AI/Public-Data/master/Data/electrification_by_province.csv'
+ebp_df = pd.read_csv(ebp_url)
+
+for col, row in ebp_df.iloc[:,1:].iteritems():
+    ebp_df[col] = ebp_df[col].str.replace(',','').astype(int)
+
+twitter_url = 'https://raw.githubusercontent.com/Explore-AI/Public-Data/master/Data/twitter_nov_2019.csv'
+twitter_df = pd.read_csv(twitter_url)
+
+# gauteng ebp data as a list
+gauteng = ebp_df['Gauteng'].astype(float).to_list()
+
+# dates for twitter tweets
+dates = twitter_df['Date'].to_list()
+
+# dictionary mapping official municipality twitter handles to the municipality name
+mun_dict = {
+    '@CityofCTAlerts' : 'Cape Town',
+    '@CityPowerJhb' : 'Johannesburg',
+    '@eThekwiniM' : 'eThekwini' ,
+    '@EMMInfo' : 'Ekurhuleni',
+    '@centlecutility' : 'Mangaung',
+    '@NMBmunicipality' : 'Nelson Mandela Bay',
+    '@CityTshwane' : 'Tshwane'
+}
+
+# dictionary of english stopwords
+stop_words_dict = {
+    'stopwords':[
+        'where', 'done', 'if', 'before', 'll', 'very', 'keep', 'something', 'nothing', 'thereupon',
+        'may', 'why', 'â€™s', 'therefore', 'you', 'with', 'towards', 'make', 'really', 'few', 'former',
+        'during', 'mine', 'do', 'would', 'of', 'off', 'six', 'yourself', 'becoming', 'through',
+        'seeming', 'hence', 'us', 'anywhere', 'regarding', 'whole', 'down', 'seem', 'whereas', 'to',
+        'their', 'various', 'thereafter', 'â€˜d', 'above', 'put', 'sometime', 'moreover', 'whoever', 'although',
+        'at', 'four', 'each', 'among', 'whatever', 'any', 'anyhow', 'herein', 'become', 'last', 'between', 'still',
+        'was', 'almost', 'twelve', 'used', 'who', 'go', 'not', 'enough', 'well', 'â€™ve', 'might', 'see', 'whose',
+        'everywhere', 'yourselves', 'across', 'myself', 'further', 'did', 'then', 'is', 'except', 'up', 'take',
+        'became', 'however', 'many', 'thence', 'onto', 'â€˜m', 'my', 'own', 'must', 'wherein', 'elsewhere', 'behind',
+        'becomes', 'alone', 'due', 'being', 'neither', 'a', 'over', 'beside', 'fifteen', 'meanwhile', 'upon', 'next',
+        'forty', 'what', 'less', 'and', 'please', 'toward', 'about', 'below', 'hereafter', 'whether', 'yet', 'nor',
+        'against', 'whereupon', 'top', 'first', 'three', 'show', 'per', 'five', 'two', 'ourselves', 'whenever',
+        'get', 'thereby', 'noone', 'had', 'now', 'everyone', 'everything', 'nowhere', 'ca', 'though', 'least',
+        'so', 'both', 'otherwise', 'whereby', 'unless', 'somewhere', 'give', 'formerly', 'â€™d', 'under',
+        'while', 'empty', 'doing', 'besides', 'thus', 'this', 'anyone', 'its', 'after', 'bottom', 'call',
+        'nâ€™t', 'name', 'even', 'eleven', 'by', 'from', 'when', 'or', 'anyway', 'how', 'the', 'all',
+        'much', 'another', 'since', 'hundred', 'serious', 'â€˜ve', 'ever', 'out', 'full', 'themselves',
+        'been', 'in', "'d", 'wherever', 'part', 'someone', 'therein', 'can', 'seemed', 'hereby', 'others',
+        "'s", "'re", 'most', 'one', "n't", 'into', 'some', 'will', 'these', 'twenty', 'here', 'as', 'nobody',
+        'also', 'along', 'than', 'anything', 'he', 'there', 'does', 'we', 'â€™ll', 'latterly', 'are', 'ten',
+        'hers', 'should', 'they', 'â€˜s', 'either', 'am', 'be', 'perhaps', 'â€™re', 'only', 'namely', 'sixty',
+        'made', "'m", 'always', 'those', 'have', 'again', 'her', 'once', 'ours', 'herself', 'else', 'has', 'nine',
+        'more', 'sometimes', 'your', 'yours', 'that', 'around', 'his', 'indeed', 'mostly', 'cannot', 'â€˜ll', 'too',
+        'seems', 'â€™m', 'himself', 'latter', 'whither', 'amount', 'other', 'nevertheless', 'whom', 'for', 'somehow',
+        'beforehand', 'just', 'an', 'beyond', 'amongst', 'none', "'ve", 'say', 'via', 'but', 'often', 're', 'our',
+        'because', 'rather', 'using', 'without', 'throughout', 'on', 'she', 'never', 'eight', 'no', 'hereupon',
+        'them', 'whereafter', 'quite', 'which', 'move', 'thru', 'until', 'afterwards', 'fifty', 'i', 'itself', 'nâ€˜t',
+        'him', 'could', 'front', 'within', 'â€˜re', 'back', 'such', 'already', 'several', 'side', 'whence', 'me',
+        'same', 'were', 'it', 'every', 'third', 'together'
+    ]
+}
+
+
 #Function 1
 def dictionary_of_metrics(items):
     """ 
@@ -29,9 +94,6 @@ This function takes in a list of integers and returns a dictionary
 
 
 #Function 3
-
-
-
 def date_parser(dates):
     '''
     Creating a list of dates only wihout the time 
@@ -63,13 +125,17 @@ def date_parser(dates):
 #Function 4
 def extract_municipality_hashtags(df):
 
-    '''
+    """
     This function takes a dataframe of tweets consisting of two columns (Tweets and Dates respectively)
     and returns a dataframe with with two new columns added: municipality; hashtags.
 
-    municipality: this column contains the municipality mentioned in the tweet as per the supplied dictionary muni_dict.
-    hashtags: this column contains the hashtags contained in the tweet.
-    '''
+    Args:
+        df (dataframe): A pandas data frame consisting of two Columns (Tweets, Dates)
+    Returns:
+        pandas dataframe with two new columns
+        municipality: this column contains the municipality mentioned in the tweet as per the supplied dictionary muni_dict.
+        hashtags: this column contains the hashtags contained in the tweet.
+    """
     df_working = df.copy(deep = True)
     finder_keys = list(mun_dict.keys())
     finder_values = list(mun_dict.values())
@@ -130,7 +196,6 @@ def number_of_tweets_per_day(df):
     byDate_copy = df_copy.groupby('Date').count().copy(deep = True)
 
     return byDate_copy
-
 #End of function 5
 
 
