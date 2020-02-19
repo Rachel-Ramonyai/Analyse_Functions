9463b93931dbf0670f0a302bf575749d8720eb2d
k-skaap <kwande.skaap@gmail.com>
2020-02-19 14:30:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='analyseModule',
-    version='1.0',
+    version='1.4',
     packages=find_packages(include=['analysepackage']),
     install_requires=['pandas', 'numpy'],
     url='https://github.com/londzy/Analyse_Functions',
